41f9ea9b9ac6c2a1f95e1a173fd2445d25ee892f
Tim Sackton <tsackton@g.harvard.edu>
2022-08-18 12:27:09 -0400

    new map

--- geojson.js
+++ geojson.js
@@ -9,7 +9,40 @@ var landmarks = {
       },
       geometry: {
         type: 'Point',
-        coordinates: [-18.640625,36.77734375]
+        coordinates: [-18.640625,37.423828]
+      }
+    },
+    {
+      type: 'Feature',
+      properties: {
+        name: 'Wellby\'s Current Location',
+        Picture: 'images/wellby1.png'
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [37.25,37.58984375]
+      }
+    },
+    {
+      type: 'Feature',
+      properties: {
+        name: 'Kenzo\'s Current Location',
+        Picture: 'images/kenzo1.png'
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [44.28125,5.6875]
+      }
+    },
+    {
+      type: 'Feature',
+      properties: {
+        name: 'Delwath\'s Current Location',
+        Picture: 'images/delwath1.png'
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [-8.71875,55.8203125]]
       }
     },
     {
@@ -20,7 +53,7 @@ var landmarks = {
       },
       geometry: {
         type: 'Point',
-        coordinates: [-9.939453125,50.701171875]
+        coordinates: [-9.939453125,51.701171875]
       }
     }
   ]
