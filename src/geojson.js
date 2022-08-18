1ab90d573e88dbe194b1057226ca500b876ff348
Tim Sackton <tsackton@g.harvard.edu>
2022-08-18 12:42:06 -0400

    oops

---
+++ src/geojson.js
@@ -0,0 +1,60 @@
+var landmarks = {
+  "type" : "FeatureCollection",
+  "features" : [
+    {
+      type: 'Feature',
+      properties: {
+        name: 'Chardon',
+        Picture: 'images/chardon1.png'
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [-18.640625,37.423828]
+      }
+    },
+    {
+      type: 'Feature',
+      properties: {
+        name: 'Wellby\'s Current Location',
+        Picture: 'images/wellby1.png'
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [37.25,37.58984375]
+      }
+    },
+    {
+      type: 'Feature',
+      properties: {
+        name: 'Kenzo\'s Current Location',
+        Picture: 'images/kenzo1.png'
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [44.28125,5.6875]
+      }
+    },
+    {
+      type: 'Feature',
+      properties: {
+        name: 'Delwath\'s Current Location',
+        Picture: 'images/delwath1.png'
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [-8.71875,55.8203125]
+      }
+    },
+    {
+      type: 'Feature',
+      properties: {
+        name: 'Nymthrax\'s Lair',
+        Picture: 'images/nymthrax1.png'
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [-9.939453125,51.701171875]
+      }
+    }
+  ]
+};
